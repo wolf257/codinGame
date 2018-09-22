9cf4573a6e5b8fb7e50717ce0e00f4e8db8030d8
Usain-mbp <wolf257@users.noreply.github.com>
2018-09-22 11:10:41 +0200

    Add chevaux de course v1

---
+++ easy/python3/chevaux_de_course.py
@@ -0,0 +1,28 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input()) #nbre chevaux
+liste_valeurs = []
+liste_classee = []
+liste_ecarts = []
+
+for i in range(n):
+    pi = int(input())
+
+    liste_valeurs.append(pi)
+
+liste_classee = liste_valeurs.copy()
+liste_classee.sort()
+
+#calcul des écarts
+for i in range(n-1):
+    ecart = abs(liste_classee[i] - liste_classee[i+1])
+    liste_ecarts.append(ecart)
+
+ecart_min = min(liste_ecarts)
+
+
+print(ecart_min)
