e3fc0700cd38073d3c7006fcaf80cdce6bf66c8a
Usain-mbp <wolf257@users.noreply.github.com>
2018-09-24 16:12:42 +0200

    Add preambles

--- easy/python3/onboarding.py
+++ easy/python3/onboarding.py
@@ -1,13 +1,11 @@
-# game loop
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 while 1:
     enemy_1 = input()  # name of enemy 1
     dist_1 = int(input())  # distance to enemy 1
     enemy_2 = input()  # name of enemy 2
     dist_2 = int(input())  # distance to enemy 2
-
-    # Write an action using print
-
-    # Enter the code here
 
     if dist_1 < dist_2:
         print(enemy_1)
